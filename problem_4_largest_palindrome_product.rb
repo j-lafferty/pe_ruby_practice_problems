e9b3088a88c56eb68ebfb16f64d780df221543f7
James Lafferty <james@j-lafferty.net>
2020-09-17 18:06:09 -0700

    Add solution

--- problem_4_largest_palindrome_product.rb
+++ problem_4_largest_palindrome_product.rb
@@ -2,3 +2,10 @@
 
 # Find the largest palindrome made from the product of two 3-digit numbers.
 
+palindrome = []
+
+[*100..999].combination(2).to_a.select do |pair| 
+  palindrome << pair.inject(:*) if pair.inject(:*).to_s == pair.inject(:*).to_s.reverse
+end
+
+p palindrome.uniq.max
