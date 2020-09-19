ed1bd4d56749c449aee330db1ebc05eba776d222
James Lafferty <james@j-lafferty.net>
2020-09-19 09:58:12 -0700

    Refactor to use standard library Prime#prime?; similar performance to previous refactored solution.

--- problem_7_10001st_prime.rb
+++ problem_7_10001st_prime.rb
@@ -2,34 +2,16 @@
 
 # What is the 10 001st prime number?
 
-def is_prime(n)
-  return n == 2 if n % 2 == 0
-  return n == 3 if n % 3 == 0
-  return n == 5 if n % 5 == 0
-
-  step, m = 4, Math.sqrt(n) + 1
-  counter = 5
-
-  # numbers divisible by neither 2 nor 3
-  # are precisely the numbers of the form 6*k ± 1
-  while counter < m
-    return false if n % counter == 0
-    step = 6 - step # step alternates between 2 and 4
-    counter += step
-  end
-
-  true
-end
+require 'prime'
 
 def nth_prime(n)
   candidate = 2
   counter = 0
 
   while counter < n
-    counter += 1 if is_prime(candidate)
+    counter += 1 if candidate.prime?
     candidate += 1
   end
-
   candidate - 1
 end
 
